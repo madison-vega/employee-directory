e969f691afac020c216d79235b5960b6654062a9
Madison Vega <madison.r.vega@gmail.com>
2021-06-09 18:20:22 -0500

    added boiler plate home page

---
+++ employee-directory/src/utils/API.js
@@ -0,0 +1,7 @@
+// get data, console log it
+// filter input box
+// sort/ click on header and sort list
+// call to api one time when page loads
+// save data in state 
+// component did mount/or hooks
+// use map function to iterate over state array
