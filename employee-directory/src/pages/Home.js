f4ace90923f9a80795ae899a4b4d478c7987e5e7
Madison Vega <madison.r.vega@gmail.com>
2021-06-09 18:27:01 -0500

    put table component to render in home.js

--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Table from '../components/Table'
 
 const Home = () => {
+
+
+
+
+
+
+
     return (
-        <div>
-            
-        </div>
+        <Table/>
     )
 }
