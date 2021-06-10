adedf2e8f1979ee6a71a0bc1e35c06a5572af15f
Madison Vega <madison.r.vega@gmail.com>
2021-06-10 16:58:35 -0500

    added description to readme

--- employee-directory/src/pages/Home.js
+++ employee-directory/src/pages/Home.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Table from '../components/Table'
 
 const Home = () => {
-
+    const getData = async () => {
+        const res = await fetch('https://randomuser.me/api/')
+        const data = await res.json();
+        console.log(data);
+    }
 
 
 
@@ -10,6 +14,6 @@ const Home = () => {
 
 
     return (
-        <Table/>
+        <Table />
     )
 }
